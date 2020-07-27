33603b13512fd6ab2baed952fadc81395afa0920
Edixon Moreno <edixon.moreno.fuentes@gmail.com>
2020-07-27 13:15:12 -0400

    Ajuste

--- longestWord.js
+++ longestWord.js
@@ -1,12 +1,54 @@
-const str = ('La noche mas oscura es la que mas enseña');
+// const str = document.querySelector('.redacción');
+// const resultado = document.querySelector('.resultado');
 
-const longestWord = (srt) => {
+/* const str = 'La noche más oscura trae mucha luz cuando acaba';
+
+
+    
+        longestWord = (srt) => {
         const strArray = str.split(' ');
         const sortedStrArray = strArray.sort((strA, strB) => {
         return strB.length - strA.length;
         }
 );
-    return sortedStrArray[0];
+    return sortedStrArray[0]; 
 }
 
+
+console.log(longestWord(str)); */
+
+// console.log(longestWord(str));
+
+/* resultado.textContent = 'La palabra más larga de tu redacción es: ';
+
 console.log(longestWord(str));
+
+console.log(str.split(' '));
+console.log(str.split(' ').length);
+
+let división = (a,b) => a/b;
+
+console.log(división(6,2)); */
+
+
+function analizar() {
+
+    
+    const boton = document.getElementById('enviar');
+    var str = document.getElementById('redaccionUsuario').value;
+        
+        longestWord = (srt) => {
+        const strArray = str.split(' ');
+        const sortedStrArray = strArray.sort((strA, strB) => {
+        return strB.length - strA.length;
+        event.preventdefault(); 
+        }
+    );
+        return sortedStrArray[0]; 
+        
+    } 
+document.getElementById('resultado').innerHTML = "La palabra más larga de tu redacción es: " + longestWord(str);
+
+
+console.log(longestWord(str));
+}
